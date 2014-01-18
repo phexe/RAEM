b1776680f63ee38b83a7a0f74996c465dff082f3
phexe <phexe@phexe.com>
2014-01-19 04:37:30 +1100

    Upload RA-Player v0.0.2.0 Base
    Uploaded the Original code for RA-Player version 0.0.2.0 to GitHub.

---
+++ RA-Player/frmMain.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace RAPlayer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lvSystems = new System.Windows.Forms.ListView();
+            this.chSystem = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvGameList = new System.Windows.Forms.ListView();
+            this.chTitle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutRAPlayerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlStatusMessage = new System.Windows.Forms.Panel();
+            this.lblStatusMessage = new System.Windows.Forms.Label();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.pnlStatusMessage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(7, 27);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lvSystems);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lvGameList);
+            this.splitContainer1.Size = new System.Drawing.Size(617, 449);
+            this.splitContainer1.SplitterDistance = 203;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // lvSystems
+            // 
+            this.lvSystems.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvSystems.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chSystem});
+            this.lvSystems.FullRowSelect = true;
+            this.lvSystems.Location = new System.Drawing.Point(3, 3);
+            this.lvSystems.MultiSelect = false;
+            this.lvSystems.Name = "lvSystems";
+            this.lvSystems.Size = new System.Drawing.Size(197, 443);
+            this.lvSystems.TabIndex = 1;
+            this.lvSystems.UseCompatibleStateImageBehavior = false;
+            this.lvSystems.View = System.Windows.Forms.View.Details;
+            this.lvSystems.SelectedIndexChanged += new System.EventHandler(this.lvSystems_SelectedIndexChanged);
+            // 
+            // chSystem
+            // 
+            this.chSystem.Text = "System";
+            this.chSystem.Width = 188;
+            // 
+            // lvGameList
+            // 
+            this.lvGameList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvGameList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chTitle});
+            this.lvGameList.FullRowSelect = true;
+            this.lvGameList.Location = new System.Drawing.Point(3, 3);
+            this.lvGameList.MultiSelect = false;
+            this.lvGameList.Name = "lvGameList";
+            this.lvGameList.Size = new System.Drawing.Size(404, 443);
+            this.lvGameList.TabIndex = 0;
+            this.lvGameList.UseCompatibleStateImageBehavior = false;
+            this.lvGameList.View = System.Windows.Forms.View.Details;
+            this.lvGameList.DoubleClick += new System.EventHandler(this.lvGameList_DoubleClick);
+            // 
+            // chTitle
+            // 
+            this.chTitle.Text = "Game";
+            this.chTitle.Width = 392;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(636, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configureToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "&Options";
+            // 
+            // configureToolStripMenuItem
+            // 
+            this.configureToolStripMenuItem.Name = "configureToolStripMenuItem";
+            this.configureToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.configureToolStripMenuItem.Text = "&Configure";
+            this.configureToolStripMenuItem.Click += new System.EventHandler(this.configureToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutRAPlayerToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "&About";
+            // 
+            // aboutRAPlayerToolStripMenuItem
+            // 
+            this.aboutRAPlayerToolStripMenuItem.Name = "aboutRAPlayerToolStripMenuItem";
+            this.aboutRAPlayerToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.aboutRAPlayerToolStripMenuItem.Text = "About RA-Player";
+            this.aboutRAPlayerToolStripMenuItem.Click += new System.EventHandler(this.aboutRAPlayerToolStripMenuItem_Click);
+            // 
+            // pnlStatusMessage
+            // 
+            this.pnlStatusMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlStatusMessage.Controls.Add(this.lblStatusMessage);
+            this.pnlStatusMessage.Location = new System.Drawing.Point(0, 0);
+            this.pnlStatusMessage.Name = "pnlStatusMessage";
+            this.pnlStatusMessage.Size = new System.Drawing.Size(636, 488);
+            this.pnlStatusMessage.TabIndex = 4;
+            // 
+            // lblStatusMessage
+            // 
+            this.lblStatusMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblStatusMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatusMessage.Location = new System.Drawing.Point(6, 140);
+            this.lblStatusMessage.Name = "lblStatusMessage";
+            this.lblStatusMessage.Size = new System.Drawing.Size(624, 176);
+            this.lblStatusMessage.TabIndex = 0;
+            this.lblStatusMessage.Text = "STATUS";
+            this.lblStatusMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 488);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.pnlStatusMessage);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RA-Player";
+            this.Load += new System.EventHandler(this.RAPlayerUI_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.pnlStatusMessage.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView lvGameList;
+        private System.Windows.Forms.ColumnHeader chTitle;
+        private System.Windows.Forms.ListView lvSystems;
+        private System.Windows.Forms.ColumnHeader chSystem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutRAPlayerToolStripMenuItem;
+        private System.Windows.Forms.Panel pnlStatusMessage;
+        private System.Windows.Forms.Label lblStatusMessage;
+    }
+}
+
