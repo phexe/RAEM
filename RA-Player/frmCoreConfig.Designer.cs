b47455285cfc7ae042c88eca14d20fa85e770f5f
phexe <phexe@phexe.com>
2014-05-20 04:09:01 +1000

    Added initial Emulation Core Configuation Dialog

---
+++ RA-Player/frmCoreConfig.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace RAPlayer
+{
+    partial class frmCoreConfig
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCoreConfig));
+            this.SuspendLayout();
+            // 
+            // frmCoreConfig
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 405);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmCoreConfig";
+            this.ShowInTaskbar = false;
+            this.Text = "Configure Emulation Core";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
